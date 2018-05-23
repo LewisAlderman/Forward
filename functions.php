db77cc407fddbe0b9600c016a8072d6838a30a67
Lewis_A <lewisalderman95@gmail.com>
2018-05-23 16:32:59 +0100

    Added 'Events' custom post type

--- functions.php
+++ functions.php
@@ -5,3 +5,37 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles(){
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+function create_custom_post_type_event(){
+	$labels = array(
+		'name' => 'Events',
+		'singular_name' => 'Event',
+		'add_new' => 'Add New',
+		'add_new_item' => 'Add New Event',
+		'edit_item' => 'Edit Event',
+		'new_item' => 'New Event',
+		'view_item' => 'View Event',
+		'search_items' => 'Search Events',
+		'not_found' => 'No events found',
+		'not_found_in_trash' => 'No events found in Trash',
+		'parent_item_colon' => '',
+	);
+	$args = array(
+		'label' => __('Events'),
+		'labels' => $labels, // from array above
+		'public' => true,
+		'can_export' => true,
+		'show_ui' => true,
+		'_builtin' => false,
+		'capability_type' => 'post',
+		'menu_icon' => 'dashicons-calendar', // from this list
+		'hierarchical' => false,
+		'rewrite' => array( "slug" => "events" ),
+		'supports'=> array('title', 'thumbnail', 'editor', 'excerpt'), 'show_in_nav_menus' => true,
+		'taxonomies' => array( 'event_category', 'post_tag')// own categories
+	);
+
+	register_post_type('events', $args);// used as internal identifier
+}
+
+add_action('init','create_custom_post_type_event');
